daabacdf9f7319be45c488541972f008a4f70d48
Cloves José <clovesjsilva8@gmail.com>
2022-11-28 23:09:19 -0300

    inicio template

--- projeto-tempo/src/app/app.module.ts
+++ projeto-tempo/src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TempoWidgetMainComponent } from './components/tempo-widget-main/tempo-widget-main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BodyComponent } from './components/template/body/body.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TempoWidgetMainComponent
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
