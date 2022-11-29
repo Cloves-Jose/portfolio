d549334c0750786cf2102ea0a2e9adfcba80fe1b
Cloves José <clovesjsilva8@gmail.com>
2022-11-29 01:06:05 -0300

    home

--- projeto-tempo/src/app/app.module.ts
+++ projeto-tempo/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BodyComponent } from './components/template/body/body.component'
+import { BodyComponent } from './components/template/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BodyComponent
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
