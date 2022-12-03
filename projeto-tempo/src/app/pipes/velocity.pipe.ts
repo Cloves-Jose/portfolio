378bdd117ada88cfb053b633db356c3ca8e07509
Cloves José <clovesjsilva8@gmail.com>
2022-12-03 18:39:06 -0300

    fim

--- projeto-tempo/src/app/pipes/velocity.pipe.ts
+++ projeto-tempo/src/app/pipes/velocity.pipe.ts
@@ -5,6 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class VelocityPipe implements PipeTransform {
 
+  /**
+   * Transforma m/s para km/h
+   * @param value 
+   * @returns 
+   */
   transform(value: number): unknown {
     return (value * 3.6).toFixed(1);
   }
